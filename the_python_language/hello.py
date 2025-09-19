794f190654bf3887eb4ce293a34f5a9858fe5241
campos_48 <oicedu48@gmail.com>
2025-09-18 22:00:28 -0300

    feat: my first class

---
+++ the_python_language/hello.py
@@ -0,0 +1,8 @@
+x = 10
+
+# print(type(x))  # ver tipo da variavel com o type()
+print("Hello World!")
+
+name = input('Digite um nome: ')
+
+print(name, 'Seja bem-vindo ao Python3.') # exibir nome digitado pelo cliente
