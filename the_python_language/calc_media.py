1ad6e8058d5a2dc8a6b5a5d3f3feb90cb33e8d0d
campos-48 <campos-48@sopmac-debian.sopmac>
2025-09-23 17:17:28 -0300

    update

--- the_python_language/calc_media.py
+++ the_python_language/calc_media.py
@@ -17,45 +17,61 @@
 # else: 
 #   print(f'Não foi aprovado com média: {media}')
 
-year = int(input('Qual o ano do seu nascimento: '))
-actualYear = 2025
-res = (actualYear - year)
+# year = int(input('Qual o ano do seu nascimento: '))
+# actualYear = 2025
+# res = (actualYear - year)
 
-print(f'Provavelmente você tem {res} anos.')
+# print(f'Provavelmente você tem {res} anos.')
 
-if res > 18: 
-  print(f'Vamos continuar...')
-  user_name = input('Como deseja ser chamado: ')
-  print(f'{user_name}, estamos feliz em tê-lo aqui!')
-  print(f'******************* Vamos jogar um jogo! *******************')
-  nextQuestion = False
+# if res > 18: 
+#   print(f'Vamos continuar...')
+#   user_name = input('Como deseja ser chamado: ')
+#   print(f'{user_name}, estamos feliz em tê-lo aqui!')
+#   print(f'******************* Vamos jogar um jogo! *******************')
+#   nextQuestion = False
 
-  print('----------------------------------------------')
-  question_0 = int(input('{user_name}, Em qual ano ocorreu 11 de setembro: '))
-  if question_0 == 2001:
-    nextQuestion = True
-    print(f'Certo!')
+#   print('----------------------------------------------')
+#   question_0 = int(input('{user_name}, Em qual ano ocorreu 11 de setembro: '))
+#   if question_0 == 2001:
+#     nextQuestion = True
+#     print(f'Certo!')
 
-  if nextQuestion:
-    question_1 = int(input('{user_name}, Em qual ano teve o Brazil como sede da copa do mundo: '))
-    if question_1 == 2014:
-      nextQuestion = True
-      print(f'Certo!')
+#   if nextQuestion:
+#     question_1 = int(input('{user_name}, Em qual ano teve o Brazil como sede da copa do mundo: '))
+#     if question_1 == 2014:
+#       nextQuestion = True
+#       print(f'Certo!')
 
-  if nextQuestion:
-    question_2 = int(input('{user_name}, Quantas pernas tem uma aranha: '))
-    if question_2 == 8:
-      nextQuestion = True
-      print(f'Certo!')
+#   if nextQuestion:
+#     question_2 = int(input('{user_name}, Quantas pernas tem uma aranha: '))
+#     if question_2 == 8:
+#       nextQuestion = True
+#       print(f'Certo!')
 
-  if nextQuestion: 
-    question_3 = int(input('{user_name}, Quantas pessoas tem ao seu redor agora: '))
-    s = question_2 + question_3 + actualYear
-    d = question_0 - question_1 
-    e = d is s
-    print(e)
-    print(f'Você morrerá em {d - s} {user_name}, TE VEJO EM BREVE!')
+#   if nextQuestion: 
+#     question_3 = int(input('{user_name}, Quantas pessoas tem ao seu redor agora: '))
+#     s = question_2 + question_3 + actualYear
+#     d = question_0 - question_1 
+#     e = d is s
+#     print('Identidade entre {s} e {d}? = {e}')
+#     print(f'Você morrerá em {d - s} {user_name}, TE VEJO EM BREVE!')
 
-  print('----------------------------------------------')
-else: 
-  print(f'Ops, vamos ter que parar por aqui!')
+#   print('----------------------------------------------')
+# else: 
+#   print(f'Ops, vamos ter que parar por aqui!')
+
+# Pegando números pares
+# for l in range(1, 11):
+#   if l % 2 == 0:
+#     print(l)
+
+# contando de 1 á 100
+# for l in range(1, 101):
+#   if l <= 100:
+#     print(f'{l} - Carneirinhos')
+
+# contando de 1 á 100, pulando de 10 em 10
+for l in range(0, 110, 10):
+  if l <= 100:
+    print(f'{l} - Carneirinhos')
+
