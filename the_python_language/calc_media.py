feb76c9587145e19ca5c5c8908ceaa3c31faf62e
campos-48 <campos-48@sopmac-debian.sopmac>
2025-09-23 16:21:31 -0300

    pratice_01

---
+++ the_python_language/calc_media.py
@@ -0,0 +1,61 @@
+
+# x = int(input('Insira o primeiro número: '))
+# print(f'Recebemos o primeiro número')
+
+# y = int(input('Insira o segundo número: '))
+# print(f'Recebemos o segundo número')
+
+# z = int(input('Insira o terceiro número: '))
+# print(f'Recebemos o terceiro número')
+
+# media = (x + y + z) / 2
+
+# if media > 6: 
+#   print(f'Foi aprovado com média: {media}')
+# elif media >= 9:
+#   print(f'Foi aprovado com uma média excelente: {media}')
+# else: 
+#   print(f'Não foi aprovado com média: {media}')
+
+year = int(input('Qual o ano do seu nascimento: '))
+actualYear = 2025
+res = (actualYear - year)
+
+print(f'Provavelmente você tem {res} anos.')
+
+if res > 18: 
+  print(f'Vamos continuar...')
+  user_name = input('Como deseja ser chamado: ')
+  print(f'{user_name}, estamos feliz em tê-lo aqui!')
+  print(f'******************* Vamos jogar um jogo! *******************')
+  nextQuestion = False
+
+  print('----------------------------------------------')
+  question_0 = int(input('{user_name}, Em qual ano ocorreu 11 de setembro: '))
+  if question_0 == 2001:
+    nextQuestion = True
+    print(f'Certo!')
+
+  if nextQuestion:
+    question_1 = int(input('{user_name}, Em qual ano teve o Brazil como sede da copa do mundo: '))
+    if question_1 == 2014:
+      nextQuestion = True
+      print(f'Certo!')
+
+  if nextQuestion:
+    question_2 = int(input('{user_name}, Quantas pernas tem uma aranha: '))
+    if question_2 == 8:
+      nextQuestion = True
+      print(f'Certo!')
+
+  if nextQuestion: 
+    question_3 = int(input('{user_name}, Quantas pessoas tem ao seu redor agora: '))
+    s = question_2 + question_3 + actualYear
+    d = question_0 - question_1 
+    e = d is s
+    print(e)
+    print(f'Você morrerá em {d - s} {user_name}, TE VEJO EM BREVE!')
+
+  print('----------------------------------------------')
+else: 
+  print(f'Ops, vamos ter que parar por aqui!')
