acb5bb204c0b0069a230ebf6c7264bee6739ec55
camposEduardo48 <camposEduardo48@users.noreply.github.com>
2025-11-04 19:04:58 -0300

    staged

--- the_python_language/tests.py
+++ the_python_language/tests.py
@@ -3,21 +3,25 @@ list = []
 # len() => serve para contar a quantidade de itens contidos dentro do objeto
 
 
+# Função para adicionar itens dentro do array fazendo o loop com o while até a condigção ser satisfeita
 def incrementNew():
-    # x = len(list)
     print("Inicio")
-    while len(list) < 10:
 
+    while len(list) < 10:
         receiveData = input("Insira item: ")
+        # Fazer verificação para ver se já existe o item adicionado no array para nao duplicar
         receivedItem = receiveData
 
         if len(receivedItem) > 0:
             list.append(receivedItem)
-            print(len(list))
+            print(f"Item - {len(list)}")
         if len(list) == 10:
-            return print(f'Temos {len(list)} itens, a condição foi satisfeita')
+            print("Finalizou!")
+            print(list)
+            return print(f"Temos {len(list)} itens, a condição foi satisfeita")
         else:
             print("********************** Dados inválidos! **********************")
+    return print(f"Temos {len(list)} itens, a condição foi satisfeita")
 
 
 incrementNew()
